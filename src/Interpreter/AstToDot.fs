4f87617d72beb50687028273456277eef2a78a8d
Ivan <ivon.klass@yandex.ru>
2021-10-21 01:37:47 +0300

    unfinished astToDot rewrite

--- src/Interpreter/AstToDot.fs
+++ src/Interpreter/AstToDot.fs
@@ -1,138 +1,116 @@
 module Interpreter.AstToDot
 
-type BinTree<'a> =
-    | Tree of 'a * BinTree<'a> * BinTree<'a>
-    | Empty
+let makeLabel labelId id label =
+    $"%s{labelId}%d{id} [shape = ellipse, label = \"%s{label}\"];\n"
 
-[<AutoOpen>]
-module BinTree =
-    let single value =
-        Tree(value, Empty, Empty)
-
-    let collectASTsToDot (expressionTrees: list<BinTree<string>>) =
-        let collectTree label (start, nodes, trans) (tree: BinTree<string>) =
-            let mutable current = start
-
-            let visit =
-                fun (nd: list<_>, tr: list<_>) rootElem ->
-                    current <- current + 1
-                    (nd @ [ $"\tx{current}" + rootElem ], tr)
-
-            let rec loop (nodes: list<string>, trans: list<string>) tree =
-                match tree with
-                | Empty -> (nodes, trans)
-                | Tree (x, Empty, Empty) -> visit (nodes, trans) x
-                | Tree (x, left, Empty) ->
-                    let nd, tr = visit (nodes, trans) x
-                    let rootNum = current
-                    let nodesL, transL = loop (nd, tr) left
-                    nodesL,
-                    (transL
-                     @ [ $"\t\"%s{label}{rootNum}\" -> \"%s{label}{rootNum + 1}\";\n" ])
-
-                | Tree (x, Empty, right) ->
-                    let nd, tr = visit (nodes, trans) x
-                    let rootNum = current
-                    let nodesR, transR = loop (nd, tr) right
-                    nodesR,
-                    (transR
-                     @ [ $"\t\"%s{label}{rootNum}\" -> \"%s{label}{rootNum + 1}\";\n" ])
-
-                | Tree (x, left, right) ->
-                    let nd, tr = visit (nodes, trans) x
-                    let rootNum = current
-                    let nodesL, transL = loop (nd, tr) left
-                    let leftLastNum = current
-                    let nodesR, transR = loop (nodesL, transL) right
-                    let trL = $"\t\"%s{label}{rootNum}\" -> \"%s{label}{rootNum + 1}\";\n"
-                    let trR = $"\t\"%s{label}{rootNum}\" -> \"%s{label}{leftLastNum + 1}\";\n"
-                    nodesR, (transR @ [ trL; trR ])
-
-            let acc = nodes, trans
-            let nodes', trans' = loop acc tree
-            current, nodes', trans'
-
-        let mutable lastCounter = 0
-        let mutable nodes, trans = [ "" ], [ "" ]
-
-        for tree in expressionTrees do
-            let _last, _nodes, _trans =
-                collectTree "x" (lastCounter, nodes, trans) tree
-
-            lastCounter <- _last
-            nodes <- _nodes
-            trans <- _trans
-
-        nodes, trans
-    
-let makeLabel label =
-    $" [shape = ellipse, label = \"%s{label}\"];\n"
-
-let rec procRegExp expr =
-    match expr with
-    | AST.RSmb symbol -> single (makeLabel (string symbol))
-    | AST.RVar (AST.Var callingName) ->
-        let varCall = makeLabel "Regex.RVar"
-        let expr = makeLabel callingName
-        Tree(varCall, single expr, Empty)
-    | AST.Opt regex ->
-        let opt = makeLabel "Regex.Opt"
-        Tree(opt, (procRegExp regex), Empty)
-    | AST.Star regex ->
-        let star = makeLabel "Regex.Star"
-        Tree(star, (procRegExp regex), Empty)
-    | AST.Seq (regex1, regex2) ->
-        let seq = makeLabel "Regex.Seq"
-        let first, second = procRegExp regex1, procRegExp regex2
-        Tree(seq, first, second)
-    | AST.Alt (regex1, regex2) ->
-        let alt = makeLabel "Regex.Alt"
-        let first, second = procRegExp regex1, procRegExp regex2
-        Tree(alt, first, second)
-    | AST.Intersect (regex1, regex2) ->
-        let intersect = makeLabel "Regex.Intersect"
-        let first, second = procRegExp regex1, procRegExp regex2
-        Tree(intersect, first, second)
-
-let rec procStatement stmt =
-    match stmt with
-    | AST.PrintToDot (AST.Var name, pathString) ->
-        let varCall = makeLabel name
-        Tree(makeLabel "PrintToDot", single varCall, single (makeLabel pathString))
-    | AST.Print (AST.Var name) ->
-        let varCall = makeLabel name
-        Tree(makeLabel "Print", single varCall, Empty)
-    | AST.VDecl (AST.Var name, expr) ->
-        let decl = makeLabel "VDecl"
-        let name = single (makeLabel name)
+let makeTransition label fromId toId =
+    $"\t\"%s{label}%d{fromId}\" -> \"%s{label}%d{toId}\";\n"
+        
+let astToDot outPath (program: AST.Program) =
+    let rec processRE (parent: int, currentExpId: int) expr: string =
         match expr with
-        | AST.FindAll (str, exp) ->
-            let findAll = makeLabel "AST.FindAll"
-            let strLabeled = makeLabel str
-            let expression = procRegExp exp
-            Tree(decl, name, Tree(findAll, single strLabeled, expression))
-        | AST.IsAcceptable (str, exp) ->
-            let isAcceptable = makeLabel "AST.IsAcceptable"
-            let strLabeled = makeLabel str
-            let expression = procRegExp exp
-            Tree(decl, name, Tree(isAcceptable, single strLabeled, expression))
-        | AST.RegExp regexp -> Tree(decl, name, procRegExp regexp)
+        | AST.RSmb symbol -> makeLabel "x" currentExpId (string symbol)
+        | AST.RVar (AST.Var callingName) ->
+            let varCall = makeLabel "x" currentExpId "Regex.RVar"
+            let varName = makeLabel "x" (currentExpId + 1) callingName
+            let transL = makeTransition "x" parent currentExpId
+            let transR = makeTransition "x" parent (currentExpId + 1)
+            String.concat "\n" [varCall; varName; transL; transR]
+        | AST.Opt regex ->
+            let optLabel = makeLabel "x" currentExpId "Regex.Opt"
+            let toThis = makeTransition "x" parent currentExpId
+            let next = processRE (currentExpId, currentExpId + 1) regex
+            String.concat "\n" [optLabel; toThis; next]
+        | AST.Star regex ->
+            let starLabel = makeLabel "x" currentExpId "Regex.Star"
+            let toThis = makeTransition "x" parent currentExpId
+            let next = processRE (currentExpId, currentExpId + 1) regex
+            String.concat "\n" [starLabel; toThis; next]
+        | AST.Seq (regex1, regex2) ->
+            let seqLabel = makeLabel "x" currentExpId "Regex.Seq"
+            let toThis = makeTransition "x" parent currentExpId
+            let first, second =
+                processRE (currentExpId, currentExpId + 1) regex1,
+                processRE (currentExpId, currentExpId + 2) regex2
+            let toLeft = makeTransition "x" currentExpId (currentExpId + 1)
+            let toRight = makeTransition "x" currentExpId (currentExpId + 2)
+            String.concat "\n" [seqLabel; toThis; first; second; toLeft; toRight]
+            
+        | AST.Alt (regex1, regex2) ->
+            let alt = makeLabel "x" currentExpId  "Regex.Alt"
+            let toThis = makeTransition "x" parent currentExpId
+            let first, second =
+                processRE (currentExpId, currentExpId + 1) regex1,
+                processRE (currentExpId, currentExpId + 2) regex2
+            let toLeft = makeTransition "x" currentExpId (currentExpId + 1)
+            let toRight = makeTransition "x" currentExpId (currentExpId + 2)
+            String.concat "\n" [alt; toThis; first; second; toLeft; toRight]
+        | AST.Intersect (regex1, regex2) ->
+            let intersect = makeLabel "x" currentExpId  "Regex.Intersect"
+            let toThis = makeTransition "x" parent currentExpId
+            let first, second =
+                processRE (currentExpId, currentExpId + 1) regex1,
+                processRE (currentExpId, currentExpId + 2) regex2
+            let toLeft = makeTransition "x" currentExpId (currentExpId + 1)
+            let toRight = makeTransition "x" currentExpId (currentExpId + 2)
+            String.concat "\n" [intersect; toThis; first; second; toLeft; toRight]
 
-let astToDot output (programAST: AST.Program) =
-    let rec statementsAsBinTrees accList statements =
+    let rec procStatement (parent: int, currId: int) (stmt: AST.Stmt) =
+        match stmt with
+        | AST.PrintToDot (AST.Var name, pathString) ->
+            let printToDotStr = makeLabel "x" currId "PrintToDot"
+            let varCall = makeLabel "x" (currId + 1) name
+            let pathStr = makeLabel "x" (currId + 2) pathString
+            let trans1 = makeTransition "x" parent currId
+            let trans2 = makeTransition "x" parent (currId + 1)
+            let trans3 = makeTransition "x" parent (currId + 2)
+            String.concat "\n" [printToDotStr; varCall; pathStr; trans1; trans2; trans3]
+        | AST.Print (AST.Var name) ->
+            let printCall = makeLabel "x" currId "Print"
+            let varCall = makeLabel "x" (currId + 1) name
+            let trans1 = makeTransition "x" parent currId
+            let trans2 = makeTransition "x" parent (currId + 1)
+            String.concat "\n" [printCall; varCall; trans1; trans2]
+        | AST.VDecl (AST.Var name, expr) ->
+            let decl = makeLabel "x" currId "VDecl"
+            let name = makeLabel "x" (currId + 1) name
+            let trans1 = makeTransition "x" parent currId
+            let trans2 = makeTransition "x" parent (currId + 1)
+            
+            let currId = currId + 1
+            
+            let inner =
+                match expr with
+                | AST.FindAll (str, exp) ->
+                    let findAll = makeLabel "x" currId "AST.FindAll"
+                    let strLabeled = makeLabel "x" (currId + 1) str
+                    let trans1 = makeTransition "x" parent currId
+                    let trans2 = makeTransition "x" parent (currId + 1)
+                    let expression = processRE (currId, currId + 2) exp
+                    let trans3 = makeTransition "x" parent (currId + 2)
+                    String.concat "\n" [findAll; strLabeled; trans1; trans2; expression; trans3]
+                
+                | AST.IsAcceptable (str, exp) ->
+                    let isAcceptable = makeLabel "x" currId "AST.IsAcceptable"
+                    let strLabeled = makeLabel "x" (currId + 1) str
+                    let trans1 = makeTransition "x" parent currId
+                    let trans2 = makeTransition "x" parent (currId + 1)
+                    let expression = processRE (currId, currId + 2) exp
+                    let trans3 = makeTransition "x" parent (currId + 2)
+                    String.concat "\n" [isAcceptable; strLabeled; trans1; trans2; expression; trans3]
+                
+                | AST.RegExp regexp ->
+                    processRE (parent, currId) regexp
+            
+            String.concat "\n" [decl; name; trans1; trans2; inner]
+    
+    let rec _go id accList statements =
         match statements with
         | [] -> accList
         | stmt :: tail ->
-            let treeOfStmt = procStatement stmt
-            statementsAsBinTrees (accList @ [ treeOfStmt ]) tail
-
-    let nodes, trans =
-        programAST
-        |> statementsAsBinTrees []
-        |> collectASTsToDot
-
-    let body =
-        System.String.Concat(nodes)
-        + System.String.Concat(trans)
-
-    System.IO.File.WriteAllText(output, $"digraph G {{\n{body}}}")
+            let treeOfStmt = procStatement (0, id)  stmt
+            _go (id + 1) (accList @ [ treeOfStmt ]) tail
+    
+    let res = _go 1 [] program
+    let res' = String.concat "\n" res
+    System.IO.File.WriteAllText(outPath, $"digraph G {{\n{ res' }}}")
